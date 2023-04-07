40a88e27ec4a0d7dde9395c6e7117b8440cd0b28
Pedro Sales Muniz <46171624+salesp07@users.noreply.github.com>
2023-04-07 16:30:28 -0700

    updated base url one more time

--- react-app/src/api/axios.js
+++ react-app/src/api/axios.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
     // baseURL: 'http://localhost:8000'
-    baseURL: 'https://good-erin-jaguar-suit.cyclic.app'
+    baseURL: 'https://giddy-colt-bonnet.cyclic.app/'
 });

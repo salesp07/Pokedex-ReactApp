31ed7140310a253d99c14873fc0e517dbd0f7c63
Pedro Sales Muniz <46171624+salesp07@users.noreply.github.com>
2023-04-07 16:41:02 -0700

    changed server link in react app again

--- react-app/src/api/axios.js
+++ react-app/src/api/axios.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
     // baseURL: 'http://localhost:8000'
-    baseURL: 'https://giddy-colt-bonnet.cyclic.app/'
+    baseURL: 'https://ill-jade-macaw-veil.cyclic.app/'
 });

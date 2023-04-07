1e1f5c46124ca50cf89c6c1dd6766bb41d9cdd21
Pedro Sales Muniz <46171624+salesp07@users.noreply.github.com>
2023-04-07 15:05:38 -0700

    changed base url for requests made from react app

--- react-app/src/api/axios.js
+++ react-app/src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:8000'
+    // baseURL: 'http://localhost:8000'
+    baseURL: 'https://good-erin-jaguar-suit.cyclic.app'
 });

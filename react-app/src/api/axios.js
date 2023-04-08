c8fe7dd6b9b2e7106deb5c017bffaf2db356bccf
Pedro Sales Muniz <46171624+salesp07@users.noreply.github.com>
2023-04-07 20:31:54 -0700

    undid base url change

--- react-app/src/api/axios.js
+++ react-app/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:8000'
-    // baseURL: 'https://ill-jade-macaw-veil.cyclic.app/'
+    // baseURL: 'http://localhost:8000'
+    baseURL: 'https://ill-jade-macaw-veil.cyclic.app/'
 });

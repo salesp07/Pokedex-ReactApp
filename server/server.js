eaa55bda0cbde59acaebf445a59184ae920d4d93
Pedro Sales Muniz <46171624+salesp07@users.noreply.github.com>
2023-04-07 17:28:32 -0700

    possible fix for db connection issue

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ mongoose.set('strictQuery', false);
 
 const connectDB = async () => {
     try {
-      await mongoose.connect(process.env.DB_STRING);
+      await mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true });
       console.log(`Connected to DB`);
     } catch (error) {
       console.log("Couldn't connect to DB: ",error);
